2d8e819bbfef8f661cf2c6a5b6e5ae476ef30019
Shotaro Ikeda <shotaro@Shotaros-MacBook-Pro.local>
2016-04-06 22:06:09 -0500

    added gitconfig, got it to work normally

--- load.sh
+++ load.sh
@@ -6,10 +6,20 @@ rm -f ~/.bash_profile
 rm -f ~/.bashrc
 rm -f ~/.slate
 rm -f ~/.vimrc
+rm -f ~/.gitconfig
 
 echo "Symlinking backed up profile code"
 # Symlink files from current directory
-ln -s bash_profile ~/.bash_profile
-ln -s bashrc ~/.bashrc
-ln -s slate_config ~/.slate
-ln -s vim_config ~/.vimrc
+ln bash_profile ~/.bash_profile
+ln bashrc ~/.bashrc
+ln slate_config ~/.slate
+ln vim_config ~/.vimrc
+ln git_config ~/.gitconfig
+
+echo "Assigning proper permissions for execute and read"
+# Set permissons rwxrwxrwx
+chmod 644 bash_profile
+chmod 644 bashrc
+chmod 644 slate_config
+chmod 644 vim_config
+chmod 644 git_config
